4a49d50562cd296e5e59c80e595a6d4c3576881a
HoonyCode <rladudgns456@naver.com>
2021-12-05 23:31:50 +0900

    Add main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("사랑합니다");
+    printf("사랑합니다");
+    printf("사랑합니다");
+    printf("사랑합니다");
+    printf("사랑합니다");
+    printf("사랑합니다");
+    printf("사랑합니다");
+    printf("사랑합니다");
+    printf("사랑합니다");
+    
+
+    return 0;
+}
